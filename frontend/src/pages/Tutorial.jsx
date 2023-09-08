7e1f90efb0d45b880fa78684fe910742a0c0ab32
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-09-08 16:10:25 -0300

    fix: retirando video do tutorial devido ao tamanho

--- frontend/src/pages/Tutorial.jsx
+++ frontend/src/pages/Tutorial.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import tutorial from '../video/tutorial-sistema-de-atualizacao-shopper.mp4'
 import { Link } from 'react-router-dom'
 
 function Tutorial() {
   return (
     <div className='flex flex-col items-center mt-10 text-2xl gap-10'>
-      <h1>Confira abaixo como utilizar esse sistema de atualização de produtos da Shopper!!</h1>
-      <video className='p-5 bg-slate-800' width={1200} height={800} controls>
-        <source src={tutorial} />
-      </video>
+      <h1>
+        Confira abaixo como utilizar esse sistema de atualização de produtos da
+        Shopper!!
+      </h1>
 
-      <Link className="text-blue-600 underline" to="/">Atualizar Produtos</Link>
+      <Link className='text-blue-600 underline' to='/'>
+        Atualizar Produtos
+      </Link>
     </div>
   )
 }
